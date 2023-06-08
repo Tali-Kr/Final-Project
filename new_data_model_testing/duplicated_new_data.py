bf53eb67dc56f36f9a6ec038cb54aea0754e0b2b
Tali-Kr <talikreynin@gmail.com>
2023-06-08 15:09:51 +0300

    XGBoost model

---
+++ new_data_model_testing/duplicated_new_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+master_df = pd.read_csv("../dataset_preparation/dt_prep_tables/pred_master_dataset.csv")
+
+# region most frequented kot
+kot_freq = master_df.pivot_table(index='kot', aggfunc={'home_key': 'count'})
+kot_freq = kot_freq.sort_values(by=['home_key'], ascending=False)
+top_kot_freq = list(kot_freq.index.values)
+kot = top_kot_freq[:6]
+kot.insert(4, '19:30')  # To skip the 5th index.
+# endregion
+
+
+def duplicate(df):
+    # Duplicates the records 6 times and changing the day of the week and the kot.
+    res = pd.DataFrame()
+    for i in range(0, 7):
+        if i == 4:  # The model didnt have any train and test records with Friday.
+            continue
+        else:
+            df_temp = df.assign(day_of_week_num=i)  # Sets the day of the week to be i in all of the records.
+            # df_temp = df_temp.assign(kot=kot[i])  # Sets the kot to be the top frequented kot in all of the records.
+            frames = [df_temp, res]
+            res = pd.concat(frames)  # Concate all the temp df into one df.
+    return res
+
+
+master_df = duplicate(master_df).reset_index()  # Reset the indeics of the df.
+master_df.drop(['index', 'Unnamed: 0'], axis='columns', inplace=True)
+
+# Creating a dictionery to change the number of the day of the week to the name of the day.
+days_dic = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+master_df['day_of_week'] = master_df['day_of_week_num'].apply(lambda x: days_dic.get(x))
+
+master_df.to_csv('dup_new_data.csv')
+print()
---
+++ new_data_model_testing/max_predictions.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+def get_max_prediction(df, code):
+    """
+    Gets the max prediction, according to the given code that is set to be the key in a dictionary.
+    :param df: dataframe. Filterd df with all the option for the different day values.
+    :param code: int. Which column (parameter) need to get the max value of.
+    :return: series. Record of the max prediction.
+    """
+    best_day_options = {1: 'home_won_pred_prob', 2: 'away_won_pred_prob', 3: 'ln(attendance)_pred'}
+    # Sets the option that was given, and will be the name of the column to extract the max value of.
+    opt = best_day_options.get(code)
+    res = pd.DataFrame(df[df[opt] == max(df[opt])])  # Filter the given df to the max value
+    return res  # Return a record (series) of the max value.
+
+
+def get_best_day(df, code):
+    """
+    Creates new data frame of the best optional day for the give paramer (code).
+    :param df: dataframe. The data that need to extract the best option for each game.
+    :param code: int. Which column (parameter) need to get the max value of.
+    :return: dataframe. For each game what is the best day to play based on the paramer that was given.
+    """
+    unique_key = list((df['home_key'].unique()))  # Df that saves the unique keys for each game.
+
+    res = []  # List to store the resulting series objects
+
+    for key in unique_key:
+        game_df = df[df['home_key'] == key]  # Filter the dataframe for each game key
+        max_prediction = get_max_prediction(game_df, code)  # Get the max prediction for the game
+        if len(max_prediction) > 1:
+            max_prediction = max_prediction.drop_duplicates(subset='Unnamed: 0')
+        res.append(max_prediction)  # Append the resulting series to the list
+
+    res_df = pd.concat(res).reset_index(drop=True)  # Concatenate the list of series into a dataframe
+    res_df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    return res_df
+
